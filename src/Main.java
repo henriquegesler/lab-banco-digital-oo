0b5e0afc6644d5cabaecbf985ed51e0195b9451b
henriquegesler <henriquegesler@gmail.com>
2022-08-15 20:51:45 -0300

    Adicionado mais um exemplo (maria)

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
 		Banco banco = new Banco();
 		Cliente venilton = new Cliente();
 		venilton.setNome("Venilton");
+		venilton.setCpf("123");
 		banco.setNome("Banco Henrique");
 		Conta cc = new ContaCorrente();
 		Conta poupanca = new ContaPoupanca();
@@ -13,15 +14,22 @@ public class Main {
 		cc.transferir(100, poupanca);
 		venilton.addConta(cc);
 		venilton.addConta(poupanca);
+		Cliente maria = new Cliente("Maria", "111");
+		Conta cm = new ContaCorrente();
+		cm.depositar(500);
+		maria.addConta(cm);
 		banco.addClientes(venilton);
+		banco.addClientes(maria);
 		System.out.println(String.format("Banco: %s", banco.getNome()));
 		Iterator<Pessoa> iterator = banco.getClientes().iterator();
 		while (iterator.hasNext()) {
 			Pessoa p = iterator.next();
-			System.out.println("Titular: " + p.getNome());
+			System.out.println("Nome do Cliente: " + p.getNome());
+			System.out.println("CPF do Cliente: " + p.getCpf());
 			for(Integer i : p.getContas().keySet()){
 				p.getContas().get(i).imprimirExtrato(p.getNome());
 			}
+			System.out.println("------------------------------");
 		}
 	}
 }
